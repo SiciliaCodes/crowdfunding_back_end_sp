f6253cf92c52a133b5b432c8c69bc387cd65b21d
Sicilia <sicilia.perumalsamy@research.uwa.edu.au>
2024-09-17 19:48:12 +0800

    committing pledge and project image changes

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -5,6 +5,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     goal = models.IntegerField()
-    image = models.URLField()
+    image = models.ImageField(upload_to='project_images/', default='default_project_image.jpg')    
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
