428045169fdcad47e0e6a89c0742dc250c43c8ce
Sicilia <sicilia.perumalsamy@research.uwa.edu.au>
2024-09-21 12:54:27 +0800

    committing views

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     goal = models.IntegerField()
-    image = models.ImageField(upload_to='project_images/', default='default_project_image.jpg')    
+    image = models.URLField()    
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
 
