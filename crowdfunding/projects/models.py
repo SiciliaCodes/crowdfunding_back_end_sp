3c499d0f31147fe820c60a78411e3619ee9c4b7c
Sicilia <sicilia.perumalsamy@research.uwa.edu.au>
2024-09-24 19:43:11 +0800

    committing model relations changes

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -12,4 +12,9 @@ class Project(models.Model):
 class Pledge(models.Model):
     amount = models.IntegerField()
     comment = models.CharField(max_length=200)
-
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
